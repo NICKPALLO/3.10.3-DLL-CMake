f1f9d44c9f472d91cf098f201a70cb0de43912af
Nikolay <yakolya19@gmail.com>
2024-06-20 14:59:47 +0400

    Second commit

--- Leaver.h
+++ Leaver.h
@@ -1,10 +1,4 @@
 #pragma once
-#ifdef LEAVER_EXPORTS
-#define LEAVER_API __declspec(dllexport)
-#else
-#define LEAVER_API __declspec(dllimport)
-#endif
-
 #include <string>
 
 namespace Leaver
@@ -12,6 +6,6 @@ namespace Leaver
 	class Leaver
 	{
 	public:
-		LEAVER_API std::string leave(std::string name);
+		std::string leave(std::string name);
 	};
 }
